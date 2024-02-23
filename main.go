4918c17635ed4fdfcb3aaf353a4b89ed4bb1d13e
AliusLeeOuO <github@mgsk.net>
2024-02-23 15:46:23 +0800

    240223

--- main.go
+++ main.go
@@ -7,29 +7,17 @@ import (
 
 func main() {
 	fmt.Println("hello world!")
-	//values := []*int{
-	//	solution.NewInt(3),
-	//	solution.NewInt(5),
-	//	solution.NewInt(1),
-	//	solution.NewInt(6),
-	//	solution.NewInt(7),
-	//	solution.NewInt(4),
-	//	solution.NewInt(2),
-	//	nil,
-	//	nil,
-	//	nil,
-	//	nil,
-	//	nil,
-	//	nil,
-	//	solution.NewInt(9),
-	//	solution.NewInt(11),
-	//	nil,
-	//	nil,
-	//	solution.NewInt(8),
-	//	solution.NewInt(10),
-	//}
-	//root := solution.CreateTree(values)
-	var a = solution.ConstructFromPrePost([]int{1, 2, 4, 5, 3, 6, 7}, []int{4, 5, 2, 6, 7, 3, 1})
+	values := []*int{
+		solution.NewInt(5),
+		solution.NewInt(8),
+		solution.NewInt(9),
+		solution.NewInt(2),
+		solution.NewInt(1),
+		solution.NewInt(3),
+		solution.NewInt(7),
+	}
+	root := solution.CreateTree(values)
+	var a = solution.KthLargestLevelSum(root, 4)
 	fmt.Printf("\nresult: %v", a)
 	//fmt.Printf("\nresult: %v", solution.MinOperations([]int{3, 2, 5, 3, 1}, 3))
 }
---
+++ solution/2583_kthLargestLevelSum.go
@@ -0,0 +1,31 @@
+package solution
+
+import "sort"
+
+func KthLargestLevelSum(root *TreeNode, k int) int64 {
+	var c []int
+	// 层序遍历
+	// 创建一个队列，用于遍历，将root入队
+	var queue = []*TreeNode{root}
+	for len(queue) != 0 {
+		sum := 0
+		var levelSize = len(queue)
+		for i := 0; i < levelSize; i++ {
+			current := queue[0]
+			queue = queue[1:]
+			sum += current.Val
+			if current.Left != nil {
+				queue = append(queue, current.Left)
+			}
+			if current.Right != nil {
+				queue = append(queue, current.Right)
+			}
+		}
+		c = append(c, sum)
+	}
+	sort.Ints(c)
+	if len(c)-k < 0 {
+		return 0
+	}
+	return int64(c[len(c)-k])
+}
